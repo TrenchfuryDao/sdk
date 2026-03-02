480b018eda337e85fecfef7972943f5e5f0bf0cc
Trenchfury <kzguitar9@gmail.com>
2026-03-02 20:15:45 +0700

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Leaderboard from "../components/Leaderboard";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Leaderboard />
+      <Footer />
+    </>
+  );
+}
